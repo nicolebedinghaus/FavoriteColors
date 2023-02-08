d0ba9c5e4ea39c64caad3193a952efafeaa607a0
nicolebedinghaus <114882146+nicolebedinghaus@users.noreply.github.com>
2023-02-08 07:52:14 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My favorite colors</h1>
+    <ul>
+      <li>Blue</li>
+      <li>Purple</li>
+      <li>Green</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
